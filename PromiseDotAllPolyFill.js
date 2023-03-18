350d57aa96c29f3a8596896de518ff78c06db24a
itsrish <abhishek@unacademy.com>
2023-03-18 15:13:27 +0530

    first commit

---
+++ PromiseDotAllPolyFill.js
@@ -0,0 +1,8 @@
+const myPromiseDotAllPolyfill = (promiseArray) => {
+	// takes in an array of promises
+	// eturns a promise
+	// the promise resolves with the result of all passed promise
+	// or
+	// reject with the error message of first rejected promise
+	// Results are returned in the same order as the promises in a given array
+}
