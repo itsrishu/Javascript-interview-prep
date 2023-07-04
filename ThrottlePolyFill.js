0ef92a5c5d2e42d6e88bc4bca58f662cbfa33c9a
itsrish <abhishek@unacademy.com>
2023-07-04 16:08:35 +0530

    throttle corrected

--- ThrottlePolyFill.js
+++ ThrottlePolyFill.js
@@ -2,10 +2,27 @@
 
 function myThrottle(cb, delay) {
 	let myLast = 0
+	let timer = null
 	return (...args) => {
 		const now = new Date().getTime()
-		if (now - myLast < delay) return
+		if (now - myLast < delay) {
+			clearTimeout(timer)
+			timer = setTimeout(() => {
+				myLast = now
+				cb(...args)
+			}, delay)
+			return
+		}
 		myLast = now
-		return cb(...args)
+		cb(...args)
 	}
 }
+
+/*
+A timer variable is introduced to keep track of the setTimeout timer.
+When a function call is made within the throttle delay, the timer is cleared using clearTimeout to prevent the delayed execution. 
+Then, a new timer is set using setTimeout to invoke the function after the delay has passed.
+This adjustment ensures that the first function call is executed immediately, 
+without any delay, while subsequent calls within the throttle delay are throttled.
+
+*/
