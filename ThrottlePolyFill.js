7cd2631f05f2fe61f24d84c11ba776b82d1c6821
itsrish <abhishek@unacademy.com>
2023-11-18 16:20:28 +0530

    th

--- ThrottlePolyFill.js
+++ ThrottlePolyFill.js
@@ -1,28 +1,28 @@
 /* WRITE YOUR OWN CUSTOM THROTTLE */
 
-function myThrottle(cb, delay) {
-	let myLast = 0
-	let timer = null
-	return (...args) => {
-		const now = new Date().getTime()
-		if (now - myLast < delay) {
-			clearTimeout(timer)
-			timer = setTimeout(() => {
-				myLast = now
-				cb(...args)
-			}, delay)
-			return
+function throttle(func, delay) {
+	let lastCallTime = 0
+
+	return function (...args) {
+		const now = Date.now()
+
+		if (now - lastCallTime >= delay) {
+			func.apply(this, ...args)
+			lastCallTime = now
 		}
-		myLast = now
-		cb(...args)
 	}
 }
 
-/*
-A timer variable is introduced to keep track of the setTimeout timer.
-When a function call is made within the throttle delay, the timer is cleared using clearTimeout to prevent the delayed execution. 
-Then, a new timer is set using setTimeout to invoke the function after the delay has passed.
-This adjustment ensures that the first function call is executed immediately, 
-without any delay, while subsequent calls within the throttle delay are throttled.
+document
+	.getElementById('normal')
+	.addEventListener('click', (e) => throttledClick)
 
-*/
+const throttledClick = throttle(handleClick, 1000)
+
+function handleClick() {
+	console.log('Button clicked!')
+}
+
+;<button id='normal' onClick={handleClick}>
+	Click
+</button>
